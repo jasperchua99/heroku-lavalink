84e5f3f04d44abf7663b700922b6cc344ecfab2e
jasperchua99 <17019686@rp.edu.sg>
2020-04-24 00:37:42 +0800

    self ping

--- bootstrap.js
+++ bootstrap.js
@@ -1,33 +1,34 @@
-  
 const fs = require('fs')
 
-let application = fs.readFileSync('./application.yml','utf8')
+let application = fs.readFileSync('./application.yml', 'utf8')
 
-if(process.env.PORT){
-	application = application.replace('DYNAMICPORT',process.env.PORT)
+if (process.env.PORT) {
+  application = application.replace('DYNAMICPORT', process.env.PORT)
 }
 
-if(process.env.PASS){
-	application = application.replace('youshallnotpass',process.env.PASS)
+if (process.env.PASS) {
+  application = application.replace('youshallnotpass', process.env.PASS)
 }
 fs.writeFileSync('./application.yml', application)
 
 const spawn = require('child_process').spawn;
 const child = spawn('java', ['-jar', 'Lavalink.jar'])
 
+require('heroku-self-ping').default("http://lavalink-0.herokuapp.com/");
+
 child.stdout.setEncoding('utf8')
 child.stderr.setEncoding('utf8')
 
-child.stdout.on('data',(data)=>{
-	console.log(data);
+child.stdout.on('data', (data) => {
+  console.log(data);
 });
 
 child.stderr.on('data', (data) => {
   console.error(data);
 });
 
-child.on('error',(error)=>{
-	console.error(error);
+child.on('error', (error) => {
+  console.error(error);
 });
 
 child.on('close', (code) => {
